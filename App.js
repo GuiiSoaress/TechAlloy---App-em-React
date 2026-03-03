1910cb212d0f46f592033c95db9c41665f1a4ff6
GUILHERME SOARES DA SILVA <guilherme.silva957@senaisp.edu.br>
2026-03-03 19:57:52 -0300

    NEW: Front Page

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { use, useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [ocorrencia, setOcorrencia] = useState("");
+  const [pin, setPin] = useState("");
+  const [pesoCromo, setPesoCromo] = useState("");
+  const [pesoNiquel, setPesoNiquel] = useState("");
+  const [pesoTotal, setPesoTotal] = useState('')
+
+  function calculaMetais(){
+    const cromo = parseFloat(pesoCromo.replace(',','.'))
+    const niquel = parseFloat(pesoNiquel.replace(',','.'))
+
+    var total = cromo + niquel
+    setPesoTotal(total)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tech Alloy - Controle de produção</Text>
+      <Text>Relatório de Ocorrências</Text>
+      <TextInput
+        placeholder="Digite a ocorrência aqui"
+        value={ocorrencia}
+        onChangeText={setOcorrencia}
+      />
+      <Text>PIN de Segurança</Text>
+      <TextInput
+        placeholder="Digite seu PIN"
+        value={pin}
+        onChangeText={setPin}
+        secureTextEntry={true}
+      />
+
+      <Button title='Envie a Ocorrência' color="#3758e7" />
+      <Text>Carga de Cromo (KG)</Text>
+      <TextInput
+        placeholder="Digite a carga de Cromo"
+        value={pesoCromo}
+        onChangeText={setPesoCromo}
+        keyboardType="numeric-pad"
+      />
+      <Text>Carga de Níquel (KG)</Text>
+      <TextInput
+        placeholder="Digite a carga de Níquel"
+        value={pesoNiquel}
+        onChangeText={setPesoNiquel}
+        keyboardType="numeric-pad"
+      />
+      <Button title="Calcular total da Liga" color='#e06919' onPress={calculaMetais} />
+      <Text>Peso Total da Carga: {pesoTotal}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +59,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
